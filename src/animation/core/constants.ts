6d845dde36b5ffbadca5244b69ab548f47fd7660
mfermoura <fernanda@paragon.com.br>
2026-02-20 14:55:12 -0300

    new files

--- src/animation/core/constants.ts
+++ src/animation/core/constants.ts
@@ -1,6 +1,6 @@
 export const SIMULATION_BASE_DATE = new Date(2025, 0, 1, 0, 0, 0)
 export const DISPLAY_TIME_SCALE = 60 // 1s real = 1 min simulado
-export const SCENARIO = "S022"
+export const SCENARIO = "S024"
 
 const STREET_STYLE = "mapbox://styles/mapbox/streets-v12"
 const SATELLITE_STYLE = "mapbox://styles/mapbox/satellite-streets-v12"
