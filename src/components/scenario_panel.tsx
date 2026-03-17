b679e88fa0aa9e5321535dc9feb50e4f420aaed7
mfermoura <fernanda@paragon.com.br>
2026-03-17 09:37:10 -0300

    new commit

--- src/components/scenario_panel.tsx
+++ src/components/scenario_panel.tsx
@@ -1,11 +1,13 @@
 'use client'
 
+import { SCENARIO_NAME } from "../animation/core/constants"
+
 export default function ScenarioPanel() {
   return (
     <div className=" fixed right-4 bottom-15 z-50 flex justify-center items-center p-6">
       <div className="bg-white rounded-2xl shadow-md px-6 py-4">
         <h2 className="text-lg font-semibold text-gray-800">
-          Cenário 2030 Sem Dragagem
+          {SCENARIO_NAME}
         </h2>
       </div>
     </div>
